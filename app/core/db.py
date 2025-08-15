0b5e725b54647d14c3b706a375e723874f23867d
Jenny Santiago <jennssant@gmail.com>
2025-08-14 23:23:22 -0600

    Move database url to .env file and add .gitignore

--- app/core/db.py
+++ app/core/db.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:123@localhost:5433/postgres"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -10,6 +14,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Dependencia para FastAPI
+
+
 def get_db():
     db = SessionLocal()
     try:
